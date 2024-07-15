e0f55dbbb64dd372a6099dd0ef94f7b360bdd471
Breno Garcia <BrenoGHeat@gmail.com>
2024-07-15 11:04:26 -0300

    atividade finalizada

---
+++ src/providers/FruitContext.jsx
@@ -0,0 +1,25 @@
+import { createContext } from "react";
+import { useState } from "react";
+
+export const FruitContext = createContext({});
+
+export const FruitProvider = ({ children }) => {
+  const [fruitList, setFruitList] = useState([]);
+
+  const addFruit = (fruit) => {
+    const newFruit = { ...fruit, id: crypto.randomUUID() };
+    setFruitList([...fruitList, newFruit]);
+  };
+
+  const removeFruit = (fruitId) => {
+    const newFruitList = fruitList.filter(fruit => fruit.id !== fruitId);
+    setFruitList(newFruitList);
+}
+
+
+  return (
+   <FruitContext.Provider value={{ fruitList , addFruit , removeFruit }}>
+    {children}
+    </FruitContext.Provider>
+  );
+};
